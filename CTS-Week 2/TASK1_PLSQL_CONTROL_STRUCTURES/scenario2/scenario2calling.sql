ad455279e71506c59eb48ff61411edec9de9fe77
PURU SAI KOUSHIK <130552858+saikoushik22@users.noreply.github.com>
2025-06-29 11:17:21 +0530

    Delete CTS-Week 2 directory

--- CTS-Week 2/TASK1_PLSQL_CONTROL_STRUCTURES/scenario2/scenario2calling.sql
+++
@@ -1,8 +0,0 @@
-CALL PromoteToVIP();
-
-SELECT CustomerID, Name, Balance, IsVIP FROM Customers WHERE IsVIP = TRUE;
-
-INSERT INTO Customers (CustomerID, Name, DOB, Balance, LastModified)
-VALUES (4, 'VIP Sample User', '1970-01-01', 15000, NOW());
-
-SELECT * FROM Customers WHERE IsVIP = TRUE;
