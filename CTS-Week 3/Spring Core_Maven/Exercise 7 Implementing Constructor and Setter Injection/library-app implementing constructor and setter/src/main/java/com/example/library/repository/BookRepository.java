6cb2588199f8354dce5b69189bfa41b915b4ab8f
PURU SAI KOUSHIK <130552858+saikoushik22@users.noreply.github.com>
2025-07-05 19:57:43 +0530

    Delete Spring Core_Maven directory

--- CTS-Week 3/Spring Core_Maven/Exercise 7 Implementing Constructor and Setter Injection/library-app implementing constructor and setter/src/main/java/com/example/library/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.library.repository;
-
-import java.util.List;
-
-public class BookRepository {
-    public List<String> findAll() {
-        return List.of("Clean Code", "Effective Java", "Spring in Action");
-    }
-}
